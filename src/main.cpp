c9d4a0a378621e8d98916acf2da948eba0a910e4
Anthony Gedeon <anthony.gedeonn@gmail.com>
2021-11-18 16:54:11 -0600

    chore: rm vscode since it was automatically generated from vscode settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,129 @@
-#include  <SDL.h>
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_sdlrenderer.h"
+#include "nfd.h"
+#include <SDL.h>
 #include <stdio.h>
 
-#if !SDL_VERSION_ATLEAST(2,0,17)
-#error Requires SDL 2.0.17! Aborting!
+#if !SDL_VERSION_ATLEAST(2, 0, 17)
+#error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
 #endif
 
-//Screen dimensions
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+int main(int, char **) {
+  // Setup SDL
+  // (Some versions of SDL before <2.0.10 appears to have performance/stalling
+  // issues on a minority of Windows systems, depending on whether
+  // SDL_INIT_GAMECONTROLLER is enabled or disabled.. updating to latest version
+  // of SDL is recommended!)
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) !=
+      0) {
+    printf("Error: %s\n", SDL_GetError());
+    return -1;
+  }
 
-int main(int argc, char* args[])
-{
-	SDL_Window* window = NULL;
+  // Setup window
+  SDL_WindowFlags window_flags =
+      (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE |
+                        SDL_WINDOW_ALLOW_HIGHDPI);
+  SDL_Window *window = SDL_CreateWindow(
+      "Game Boy Emulator", SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
 
-	SDL_Surface* screenSurface = NULL;
+  // Setup SDL_Renderer instance
+  SDL_Renderer *renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+  if (renderer == NULL) {
+    SDL_Log("Error creating SDL_Renderer!");
+    return false;
+  }
+  // SDL_RendererInfo info;
+  // SDL_GetRendererInfo(renderer, &info);
+  // SDL_Log("Current SDL_Renderer: %s", info.name);
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		window = SDL_CreateWindow("Emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_ERROR: %s\n", SDL_GetError());
-		}
-		else
-		{
-			SDL_Event e;
-			while (true)
-			{
-				SDL_PollEvent(&e);
-				if (e.type == SDL_QUIT)
-				{
-					SDL_Log("Program quit after %i ticks", e.quit.timestamp);
-					break;
-				}
-				screenSurface = SDL_GetWindowSurface(window);
-				SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-				SDL_UpdateWindowSurface(window);
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  (void)io;
+  // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable
+  // Keyboard Controls io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; //
+  // Enable Gamepad Controls
 
-			}
-		}
-	}
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+  // ImGui::StyleColorsClassic();
 
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+  // Setup Platform/Renderer backends
+  ImGui_ImplSDL2_InitForSDLRenderer(window);
+  ImGui_ImplSDLRenderer_Init(renderer);
 
-	return 0;
+
+  // Our state
+  bool show_demo_window = true;
+  bool show_another_window = false;
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+  // Main loop
+  bool done = false;
+  while (!done) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      ImGui_ImplSDL2_ProcessEvent(&event);
+      if (event.type == SDL_QUIT)
+        done = true;
+      if (event.type == SDL_WINDOWEVENT &&
+          event.window.event == SDL_WINDOWEVENT_CLOSE &&
+          event.window.windowID == SDL_GetWindowID(window))
+        done = true;
+    }
+
+    // Start the Dear ImGui frame
+    ImGui_ImplSDLRenderer_NewFrame();
+    ImGui_ImplSDL2_NewFrame(window);
+    ImGui::NewFrame();
+    if (ImGui::BeginMainMenuBar()) {
+      if (ImGui::BeginMenu("File")) {
+        if (ImGui::MenuItem("Open ROM")) {
+          nfdchar_t *outPath;
+          nfdfilteritem_t filterItem = {"GB ROMs", "nes"};
+          nfdresult_t res = NFD_OpenDialog(&outPath, &filterItem, 1, NULL);
+          if (res == NFD_OKAY) {
+            printf("ROM file opened: %s\n", (char *)outPath);
+          } else if (res == NFD_CANCEL) {
+            printf("User cancelled opening ROM!\n");
+          } else {
+            printf("Error opening ROM!: %s\n", NFD_GetError());
+          }
+        }
+        ImGui::EndMenu();
+      }
+      if (ImGui::BeginMenu("Tools"))
+      {
+          ImGui::EndMenu();
+      }
+      ImGui::EndMainMenuBar();
+    }
+
+    ImGui::BeginChild("game");
+    ImGui::EndChild();
+
+    ImGui::Render();
+    SDL_SetRenderDrawColor(
+        renderer, (Uint8)(clear_color.x * 255), (Uint8)(clear_color.y * 255),
+        (Uint8)(clear_color.z * 255), (Uint8)(clear_color.w * 255));
+    SDL_RenderClear(renderer);
+    ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
+    SDL_RenderPresent(renderer);
+  }
+
+  // Cleanup
+  ImGui_ImplSDLRenderer_Shutdown();
+  ImGui_ImplSDL2_Shutdown();
+  ImGui::DestroyContext();
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
 }
