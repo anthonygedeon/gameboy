eaf7665b1deac7a11504d22c924d404105f39ac2
anthonygedeon <anthony.gedeonn@gmail.com>
2026-04-23 10:40:42 -0500

    Fix: test stub pass

--- test/test.cpp
+++ test/test.cpp
@@ -9,6 +9,6 @@ uint32_t factorial( uint32_t number ) {
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( factorial( 1) == 1 );
     REQUIRE( factorial( 2) == 2 );
-    REQUIRE( factorial( 3) == 7 );
+    REQUIRE( factorial( 3) == 6 );
     REQUIRE( factorial(10) == 3'628'800 );
 }
