c40ef197ff24d95bfec4dc78f9627c159ad36b7d
Bennett Smrdel <fireknights318@gmail.com>
2026-04-27 20:30:00 -0500

    feat: Start basic shader header and implimentation

---
+++ src/rendering/shaders/shader.h
@@ -0,0 +1,22 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <string>
+
+
+class Shader {
+public:
+    Shader(const char* vertexPath, const char* fragmentPath);
+    void use() const;
+
+    void setBool(const std::string &name, bool value) const;  
+    void setInt(const std::string &name, int value) const;   
+    void setFloat(const std::string &name, float value) const;
+
+private:
+    unsigned int id;
+
+    unsigned int compileShader(...);
+};
+
+#endif
