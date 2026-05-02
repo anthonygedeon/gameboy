83f34fa00f6b5152bc004aad3ac72589b2a90f1b
Bennett Smrdel <fireknights318@gmail.com>
2026-05-02 09:47:40 -0500

    chore: Cleanup header file

--- src/rendering/shaders/shader.cpp
+++ src/rendering/shaders/shader.cpp
@@ -30,7 +30,7 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
         fShaderFile.close();
 
         vertexCode   = vShaderStream.str();
-        fragmentCode = fShaderStream.str();		
+        fragmentCode = fShaderStream.str();
     }
     catch(const std::ifstream::failure& e)
     {
@@ -98,18 +98,18 @@ void Shader::use() const
 
 
 void Shader::setBool(const std::string &name, bool value) const
-{         
-    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); 
+{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
 }
 
 
 void Shader::setInt(const std::string &name, int value) const
-{ 
-    glUniform1i(glGetUniformLocation(ID, name.c_str()), value); 
+{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 }
 
 
 void Shader::setFloat(const std::string &name, float value) const
-{ 
+{
     glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
 }
--- src/rendering/shaders/shader.h
+++ src/rendering/shaders/shader.h
@@ -8,11 +8,11 @@ class Shader {
 public:
     Shader(const char* vertexPath, const char* fragmentPath);
     ~Shader();
-    
+
     void use() const;
 
-    void setBool(const std::string &name, bool value) const;  
-    void setInt(const std::string &name, int value) const;   
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
     void setFloat(const std::string &name, float value) const;
 
 private:
