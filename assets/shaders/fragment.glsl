2bad5f4550bfbbadf16a98292f3bb315c5de80fe
Bennett Smrdel <fireknights318@gmail.com>
2026-05-07 19:59:36 -0500

    feat: Add green color on top of all textures

--- assets/shaders/fragment.glsl
+++ assets/shaders/fragment.glsl
@@ -7,5 +7,10 @@ uniform sampler2D texture_0;
 
 void main()
 {
-    FragColor = texture(texture_0, TexCoord);
+    vec4 color = texture(texture_0, TexCoord);
+
+    float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
+    vec3 green = vec3(0.0, 1.0, 0.0);
+
+    FragColor = vec4(mix(color.rgb, green * gray, 0.6), color.a);
 }
